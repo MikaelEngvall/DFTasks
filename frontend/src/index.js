885734a60f4a3425803b06ad253fbe438ccada0a
Mikael Engvall <mikael.engvall.me@gmail.com>
2025-02-12 02:59:01 +0100

    Add AuthProvider and ThemeProvider to application root
    - Wrap App component with AuthProvider and ThemeProvider
    - Integrate context providers in the main application entry point
    - Maintain existing ToastContainer and I18nextProvider configurations

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,14 +8,20 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
+import { AuthProvider } from "./context/AuthContext";
+import { ThemeProvider } from "./context/ThemeContext";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <I18nextProvider i18n={i18n}>
-      <ToastContainer bodyStyle={{ fontFamily: "Roboto" }} />
-      <App />
+      <AuthProvider>
+        <ThemeProvider>
+          <ToastContainer bodyStyle={{ fontFamily: "Roboto" }} />
+          <App />
+        </ThemeProvider>
+      </AuthProvider>
     </I18nextProvider>
   </React.StrictMode>
 );
