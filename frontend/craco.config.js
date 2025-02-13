0dab1e0fcede778c86591b210a25019932e5f9d4
Mikael Engvall <mikael.engvall.me@gmail.com>
2025-02-13 13:13:38 +0100

    Remove mobile and frontend project artifacts
    - Delete DFTasksMobile directory containing Expo React Native project
    - Remove frontend project files and components
    - Clean up unnecessary configuration and source files
    - Simplify project structure by removing unused code

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -2,7 +2,6 @@ const path = require("path");
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const CompressionPlugin = require("compression-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
-const { GenerateSW } = require("workbox-webpack-plugin");
 
 module.exports = {
   webpack: {
@@ -33,92 +32,50 @@ module.exports = {
             priority: -20,
             reuseExistingChunk: true,
           },
-          // Separata chunks för stora paket
-          reactVendor: {
-            test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
-            name: "vendor-react",
-            chunks: "all",
-          },
-          utilityVendor: {
-            test: /[\\/]node_modules[\\/](lodash|date-fns|axios)[\\/]/,
-            name: "vendor-utilities",
-            chunks: "all",
-          },
-          i18nVendor: {
-            test: /[\\/]node_modules[\\/](i18next|react-i18next)[\\/]/,
-            name: "vendor-i18n",
-            chunks: "all",
-          },
         },
       };
 
-      // Aktivera tree shaking
-      webpackConfig.optimization.usedExports = true;
-      webpackConfig.optimization.sideEffects = true;
-
-      // Lägg till Terser för minifiering
-      webpackConfig.optimization.minimize = true;
-      webpackConfig.optimization.minimizer = [
-        new TerserPlugin({
-          terserOptions: {
-            compress: {
-              drop_console: true,
-            },
-            format: {
-              comments: false,
-            },
-          },
-          extractComments: false,
-        }),
-      ];
-
-      // Lägg till plugins för produktion
+      // Optimeringar för produktion
       if (env === "production") {
+        webpackConfig.optimization = {
+          ...webpackConfig.optimization,
+          minimize: true,
+          minimizer: [
+            new TerserPlugin({
+              terserOptions: {
+                compress: {
+                  drop_console: true,
+                },
+              },
+            }),
+          ],
+        };
+
         webpackConfig.plugins.push(
           new CompressionPlugin({
             algorithm: "gzip",
             test: /\.(js|css|html|svg)$/,
             threshold: 10240,
             minRatio: 0.8,
-          }),
-          new BundleAnalyzerPlugin({
-            analyzerMode: "disabled",
-            generateStatsFile: true,
-            statsFilename: "bundle-stats.json",
-          }),
-          new GenerateSW({
-            clientsClaim: true,
-            skipWaiting: true,
-            maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
-            runtimeCaching: [
-              {
-                urlPattern: /^https:\/\/api\./,
-                handler: "StaleWhileRevalidate",
-                options: {
-                  cacheName: "api-cache",
-                  expiration: {
-                    maxEntries: 50,
-                    maxAgeSeconds: 5 * 60, // 5 minuter
-                  },
-                },
-              },
-              {
-                urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
-                handler: "CacheFirst",
-                options: {
-                  cacheName: "images",
-                  expiration: {
-                    maxEntries: 60,
-                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 dagar
-                  },
-                },
-              },
-            ],
           })
         );
+
+        if (process.env.ANALYZE) {
+          webpackConfig.plugins.push(
+            new BundleAnalyzerPlugin({
+              analyzerMode: "static",
+              reportFilename: "bundle-report.html",
+            })
+          );
+        }
       }
 
       return webpackConfig;
+    },
+  },
+  style: {
+    postcss: {
+      plugins: [require("tailwindcss"), require("autoprefixer")],
     },
   },
 };
